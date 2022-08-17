78c0ac2708b8e4dcdce868db8bfcdf9cbb2fc580
Kundan <kundan kamble>
2022-08-17 13:46:02 -0800

    Password Rule-1

--- RegexPattern/Program.cs
+++ RegexPattern/Program.cs
@@ -35,7 +35,12 @@ namespace RegexPattern
             string Phone_Number = Console.ReadLine();
             person.Phonenumber(Phone_Number);
 
+            //UC5
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Enter The Password: ");
+            string Password = Console.ReadLine();
+            person.Password(Password);
+
         }
     }
 }
-
--- RegexPattern/Validation.cs
+++ RegexPattern/Validation.cs
@@ -12,7 +12,8 @@ namespace RegexPattern
         public string FirstName = ("^[A-Z]{1}[A-Za-z]{3,}$");
         public string LastName = ("^[A-Z]{1}[A-Za-z]{3,}$");
         public string Email = ("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
-        public string PhoneNumber = ("^[0-9]{2}[ ][5-9]{1}[0-9]{9}$");
+        public string PhoneNumber = ("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
+        public string PassWord = ("^[A-z0-9a-z@#&*+]{8,}$");
 
         public void ValidateFirstName(string firstName)
         {
@@ -42,5 +43,13 @@ namespace RegexPattern
             else
                 Console.WriteLine("Invalid Phone_Number");
         }
+        public void Password(string Password)
+        {
+            if (Regex.IsMatch(Password, PassWord))
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
+        }
     }
 }
+
