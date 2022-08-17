e13c4b32f03f873b67ac0f15f637c79ec871eed1
Kundan <kundan kamble>
2022-08-17 13:49:40 -0800

    Password Rule-3

--- RegexPattern/Program.cs
+++ RegexPattern/Program.cs
@@ -35,11 +35,12 @@ namespace RegexPattern
             string Phone_Number = Console.ReadLine();
             person.Phonenumber(Phone_Number);
 
-            //UC6
+            //UC7
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Enter The Password: ");
             string Password = Console.ReadLine();
             person.Password(Password);
+
 
         }
     }
