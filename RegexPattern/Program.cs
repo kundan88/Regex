3c71eadf30ea526c726a26b57bfb0f7f276548dd
Kundan <kundan kamble>
2022-08-17 13:33:26 -0800

    Valid First Name

---
+++ RegexPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1
+            Validation person = new Validation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstName = Console.ReadLine();
+            person.ValidateFirstName(firstName);
+
+        }
+    }
+}
