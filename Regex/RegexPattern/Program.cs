5623a7c5171825ff7400df161a4df610ede335c7
Kundan <kundan kamble>
2022-08-24 18:49:11 -0800

    JUnit Test to validate

--- Regex/RegexPattern/Program.cs
+++ Regex/RegexPattern/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-
-namespace RegexPattern
+﻿namespace RegexPattern
 {
     public class Program
     {
@@ -20,8 +13,8 @@ namespace RegexPattern
             //UC2
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Enter The Last Name: ");
-            string lastName = Console.ReadLine();
-            person.ValidLastName(lastName);
+            string lastname = Console.ReadLine();
+            person.ValidateLastName(lastname);
 
             //UC3
             Console.WriteLine("-----------------------------------");
@@ -35,17 +28,29 @@ namespace RegexPattern
             string Phone_Number = Console.ReadLine();
             person.Phonenumber(Phone_Number);
 
-            //UC8
+            //UC5
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Enter The Password: ");
             string Password = Console.ReadLine();
             person.Password(Password);
 
-            //UC9
+            //UC6
             Console.WriteLine("-----------------------------------");
-            Console.WriteLine("Enter The Email_ID: ");
-            string Emails = Console.ReadLine();
-            person.AllEmails(Emails);
+            Console.WriteLine("Enter The Password: ");
+            string PasswordRule2 = Console.ReadLine();
+            person.Password(PasswordRule2);
+
+            //UC7
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Enter The Password: ");
+            string PasswordRule3 = Console.ReadLine();
+            person.Password(PasswordRule3);
+
+            //UC8
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Enter The Password: ");
+            string PasswordRule4 = Console.ReadLine();
+            person.Password(PasswordRule4);
 
         }
     }
